4bc801c044a10263213912b5247c6cae7030e70b
AmgadMagdyHalim <amgad.magdy.halim@gmail.com>
2024-10-12 15:04:01 +0300

    List, Button & Alert

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import Message from './Message';
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
+
+let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+const handleSelectItem = (item: string) => {
+  console.log(item);
+};
 
 const App = () => {
+  const [alertVisible, setAlertVisiblity] = useState(false);
+
   return (
-    <>
-      <Message/>
-    </>
+    <div>
+      <ListGroup
+        items={items}
+        heading={"Cities"}
+        onSelectItem={handleSelectItem}
+      />
+      <Alert>
+        Hello <span> World</span>
+      </Alert>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisiblity(false)}>My Alert</Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisiblity(true)}>
+        My Button
+      </Button>
+    </div>
   );
-}
+};
 
 export default App;
--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -1,15 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const Message = () => {
-
-const name=''
-if (name)
-  return (
-    <h1>
-      Hello {name}
-    </h1>
-  );
-  return <h1>Hello World</h1>
-}
+  const name = "";
+  if (name) return <h1>Hello {name}</h1>;
+  return <h1>Hello World</h1>;
+};
 
 export default Message;
